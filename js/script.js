f21a8c698ac936f7b9e5da1461d6c44d0ce111ca
Rosio <rosioilene@yahoo.com>
2019-10-26 19:03:39 -0700

    Initial commit

--- js/script.js
+++ js/script.js
@@ -3,58 +3,46 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 Rosio Dorson
 ******************************************/
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-// I need variables two global variables.
-
-const studentList;
+const studentList = document.querySelector('.student-list').children;
 const itemsPerPage = 10;
-console.log(studentList);
-/***
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
 
 function showPage(list, page) {
-   const startIndex = (page * itemsPerPage);
-   const endIndex = page * itemsPerPage;
-   for (let i = 0; i < list.lenth; i += 1) {
-      let li = list[i];
+   const startIndex = (page * itemsPerPage) - itemsPerPage;
+   const endIndex = page * itemsPerPage - 1;
+   for (let i = 0; i < list.length; i += 1) {
       if (i >= startIndex && i <= endIndex) {
-         li.style.display = '';
+         list[i].style.display = '';
       } else {
-         li.style.display = 'none';
+         list[i].style.display = 'none';
       }  
    }
 }
 
+function appendPageLinks (list) {
+   const pages = studentList.length / itemsPerPage; 
+   const newDiv = document.createElement('newDiv');
+   newDiv.className = 'pagination';
+   const div = document.querySelector('.page');
+   div.appendChild = (newDiv);
+   const ul = document.createElement('ul');
+   newDiv.appendChild = (ul);
+   for (let i = 1; i <= pages; i++) {
+      const li = document.createElement('li');
+      ul.appendChild(li);
+      const a = document.createElement('a');
+      li.appendChild(a);
+      a.textContent = i;
+      a.addEventListener('click', (event) => {
+      showPage(studentList, i);
+         for (i = 0; i <= pages + 1; i ++) {
+           const active = document.querySelectorAll('a');
+           active.className = ''
+           event.target.className = 'active'
+         }
+      })
+   }  
+}
 
-/*** 
-   Create the `appendPageLinks function` to generate, append, and add 
-   functionality to the pagination buttons.***/
 
-
-
-
-
-
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+showPage(studentList, 1);
+appendPageLinks();
