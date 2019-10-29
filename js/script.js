6485ae77517a2ccd354da326b3dfaaadb0b7d10a
Rosio <rosioilene@yahoo.com>
2019-10-28 19:42:21 -0700

    Fixed the pagination links and added code comments

--- js/script.js
+++ js/script.js
@@ -3,13 +3,19 @@ Treehouse Techdegree:
 FSJS project 2 - List Filter and Pagination
 Rosio Dorson
 ******************************************/
-const studentList = document.querySelector('.student-list').children;
-const itemsPerPage = 10;
 
+// Create 2 global variables to store the student list and the number of students needed per page. 
+
+const studentList = document.querySelectorAll('.student-item');
+const itemsPerPage = 10;
+console.log(studentList);
+
+// Create a function to hid all the students except the number given per page. Here it is 10 but the last page will have 4 since there are 54 students.
 function showPage (list, page) {
    const startIndex = (page * itemsPerPage) - itemsPerPage;
-   const endIndex = page * itemsPerPage - 1;
-   for (let i = 0; i < list.length; i += 1) {
+   const endIndex = (page * itemsPerPage) - 1;
+   // The loop starts with the first student or "0".
+   for (let i = 0; i < list.length; i ++) {
       if (i >= startIndex && i <= endIndex) {
          list[i].style.display = '';
       } else {
@@ -18,17 +24,17 @@ function showPage (list, page) {
    }
 }
 
-
-function appendPageLinks (list) {
-   const pages = studentList.length / itemsPerPage; 
+// Create the appendPageLinks function to add and append the pagination links. 
+appendPageLinks = (list) => {
+   const pages = Math.ceil(list.length / itemsPerPage); 
+   console.log(pages);
    const newDiv = document.createElement('div');
    newDiv.className = 'pagination';
    const appendDiv = document.querySelector('.page');
    appendDiv.appendChild(newDiv);
    const ulDiv = document.createElement('ul');
    newDiv.appendChild(ulDiv);
-
-    for (let i = 1; i <= pages; i++) {
+   for (let i = 1; i <= pages; i++) {
       const li = document.createElement('li');
       ulDiv.appendChild(li);
       const a = document.createElement('a');
@@ -36,19 +42,15 @@ function appendPageLinks (list) {
       a.textContent = i;
       const allButtons = document.querySelectorAll('a');
       a.addEventListener('click', (event) => {
-         for (let i = 0; i < allButtons.length; i++) {
-        
+      showPage(studentList, i);
+         for (let i = 1; i < allButtons.length; i++) {
          allButtons[i].className = '';
-      
          event.target.className = 'active';
-         showPage(studentList, i);
          }
       })
    }
 }
 
-
-
-
+// Call the showPage and appendPagelinks functions.
 showPage(studentList, 1);
-appendPageLinks();
+appendPageLinks(studentList);
